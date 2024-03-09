cdccbfd772444acc13f681a3a0e6aac8899202b7
Sourajit Majumder <sourajitm19@gmail.com>
2024-03-09 22:52:24 +0530

    Queue in C

--- Queue/Queue.c
+++ Queue/Queue.c
@@ -5,23 +5,25 @@ int queue[MAX_SIZE];
 int front = -1;
 int rear = -1;
 
-void add(int queue[], int elem){
-    if(front==-1 && rear==-1){
-        front = 0;
-        rear = 0;
-        queue[rear] = elem;
-        printf("%d is pushed into Queue\n", elem);
-    }else if(rear < MAX_SIZE-1){
-        queue[++rear] = elem;
-        printf("%d is pushed into Queue\n", elem);
-    }else{
+void add(int queue[]){
+    if(rear == MAX_SIZE-1){
         printf("Queue is Overflow!!\n");
+        return;
     }
+    if(rear == -1){
+        front = 0;
+    }
+    int elem;
+    printf("Enter the element to be inserted: ");
+    scanf("%d", &elem);
+    queue[++rear] = elem;
+    printf("%d is pushed into Queue\n", elem);
+    
 }
 
 void remove_element(int queue[]){
     if(rear==-1 && front==-1){
-        printf("Queue is empty!!");
+        printf("Queue is Underflow!!\n");
     }else if(front==rear){
         printf("Removed element is %d \n", queue[rear]);
         front=-1;
@@ -52,12 +54,9 @@ int main(){
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        int elem;
         switch (choice) {
             case 1:
-            printf("Enter the element to be inserted: ");
-            scanf("%d", &elem);
-            add(queue, elem);
+            add(queue);
             break;
 
             case 2:
